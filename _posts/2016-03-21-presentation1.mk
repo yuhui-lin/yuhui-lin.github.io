87ecfe5dd8577ad8cdf417a4566a5c2947dec89b
volantfish <linyuhuihaha@gmail.com>
2016-03-21 09:43:20 -0600

    test pdf

---
+++ _posts/2016-03-21-presentation1.mk
@@ -0,0 +1,9 @@
+---
+title: "Creation of a Landscape: The Logic or Urbanization in the PRC"
+description: NYU East Asian Studies Department, MA Thesis Defense
+date: 2013-11-01
+layout: pdf
+categories: writing
+permalink: /writing/creation-of-a-landscape/
+---
+<iframe src="https://drive.google.com/file/d/0B0CvXTlo6UncNnExX0R3NWN1UWM/preview" width="100%" height="100%"></iframe>
