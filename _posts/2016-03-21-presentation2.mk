3d2753306404479a43bc8373c738dfbb3dabdd79
volantfish <linyuhuihaha@gmail.com>
2016-03-21 10:10:28 -0600

    pdf

--- _posts/2016-03-21-presentation2.mk
+++ _posts/2016-03-21-presentation2.mk
@@ -1,8 +1,7 @@
 ---
-title: "CPSC test"
-description: NYU East Asian Studies Department, MA Thesis Defense
+title: "CPSCtest"
 date: 2016-11-01
-layout: default
+layout: pdf
 ---
 
 <iframe src="https://drive.google.com/file/d/0B0CvXTlo6UncNnExX0R3NWN1UWM/preview" width="640" height="480"></iframe>
