2f23a40636bab49137e375d8fb572c4bea3cd51e
volantfish <linyuhuihaha@gmail.com>
2016-03-21 09:56:48 -0600

    pdf test

--- _posts/2016-03-21-presentation1.mk
+++ _posts/2016-03-21-presentation1.mk
@@ -5,4 +5,4 @@ date: 2013-11-01
 layout: default
 ---
 
-<iframe src="https://drive.google.com/file/d/0B0CvXTlo6UncNnExX0R3NWN1UWM/preview" width="640" height="480"></iframe>
+<iframe src="https://drive.google.com/file/d/0B0CvXTlo6UncNnExX0R3NWN1UWM/preview" width="100%" height="100%"></iframe>
